7f559addf93b3bc2c1bd1864a1005b0b54382d4b
MrDeveloperrr <mrdemote@gmail.com>
2024-02-22 12:52:50 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from 'express'
+const app = express()
+
+const PORT = 3000
+
+ app.use(express.json()) 
+
+app.listen(PORT, ()=> {
+    console.log(`Server started on port ${PORT}`)
+})
+
+app.get('/', (req, res)=> {
+    res.status(212).send("Domain Expansion")
+
+})
+
+app.get('/shop', (req, res)=> {
+    res.status(232).send("<a href='/'> Home</a>")
+
+    
+})
+app.get('/shop/:id', (req, res)=> {
+    const data = req.params
+    res.status(232).send(`<a href='/'> Home ${data.id}</a>`)
+})
