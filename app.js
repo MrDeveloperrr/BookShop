102b352f5c66f067879573e93a4ac99725e2bec8
MrDeveloperrr <mrdemote@gmail.com>
2024-03-07 13:21:55 -0400

    Crud Operations

--- app.js
+++ app.js
@@ -1,25 +1,124 @@
-import express from 'express'
+import express, { response } from 'express'
+import {PORT, MongoDBURL} from './config.js'
+import {MongoClient, ObjectId, ServerApiVersion} from "mongodb"
 const app = express()
 
-const PORT = 3000
 
  app.use(express.json()) 
+
+ const client = new MongoClient (MongoDBURL, {
+    serverApi: {
+         version: ServerApiVersion.v1,
+         strict: true,
+         deprecationErrors: true,
+        }
+    });
+    
+    const bookDB = client.db("myBookShop")
+    const myBooks = bookDB.collection("booksCollection")
+    
 
 app.listen(PORT, ()=> {
     console.log(`Server started on port ${PORT}`)
 })
 
 app.get('/', (req, res)=> {
-    res.status(212).send("Domain Expansion")
+  return  res.status(202).send("Domain Expansion")
 
 })
 
 app.get('/shop', (req, res)=> {
-    res.status(232).send("<a href='/'> Home</a>")
-
+    myBooks.find().toArray()
+    .then(response=>{
+        //console.log(response)
+        return res.status(202).send(response)
+    })
+    .catch(err=>console.log(err))
+    
+    //return  res.status(202).send("<a href='/'> Home</a>")
     
 })
 app.get('/shop/:id', (req, res)=> {
     const data = req.params
-    res.status(232).send(`<a href='/'> Home ${data.id}</a>`)
+
+const filter = {
+    "_id" : new ObjectId(data.id) 
+    }
+
+    myBooks.findOne(filter)
+    .then(response=>{
+        //console.log(response)
+        return res.status(202).send(response)
+        })
+        .catch(err=>console.log(err))
+        
 })
+
+
+
+    
+
+// return  res.status(202).send(`<a href='/'> Home ${data.id}</a>`)
+
+app.post('/admin/savebook', (req, res)=> {
+    const data = req.body
+    if (!data.title)
+        return res.status(202).send("No title found.")
+    if (!data.author)
+    return res.status(202).send("No author found.")
+if (!data.price)
+return res.status(202).send("No price found.")
+
+myBooks.insertOne(data, (error, response)=>{
+    if(error){
+        console.log("An error occurred!")
+        return res.sendStatus(202)
+    }
+
+ })
+    return res.status(202).send(JSON.stringify(data) )
+})
+    
+  app.delete ('/admin/remove/:id', (req ,res)=>{
+  
+  const data = req.params
+
+    const filter ={
+        "_id" : new ObjectId(data.id)
+    }
+
+  myBooks.deleteOne(filter)
+  .then(response=>{
+      //console.log(response)
+      return res.status(202).send(response)
+      })
+      .catch(err=>console.log(err))
+  })  
+    
+    
+  app.put('/admin/update/:id', (req, res)=>{
+
+      
+      const data = req.params
+      const docData = req.body
+      
+      const filter = {
+          "_id" : new ObjectId(data.id)
+        }
+        const updDoc = {
+            $set : {
+                ...docData
+            }
+        }
+        
+        
+        myBooks.updateOne(filter, updDoc)
+        .then (response=>{
+
+           return res.status(202).send(response)
+        })
+        .catch(err=>console.log(err))
+        
+        
+    })
+
